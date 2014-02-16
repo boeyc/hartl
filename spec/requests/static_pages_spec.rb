2d616497529e62ab21e2672f91fe2dea27500a55
Clarence Boey <clarenceboey@gmail.com>
2014-02-16 15:56:57 -0500

    Finished Static_pages with title and tests

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+
+    it "should have title 'home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
+  end
+
+  describe "Help Page" do
+
+    it "should have 'Admin Credentials'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('clarence.boey@gmail.com')
+    end
+
+    it "should have title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+
+  describe "About Page" do
+
+    it "should have 'The Goal'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('The Goal')
+    end
+
+    it "should have title 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+end
